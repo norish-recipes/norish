0a93f20eee1cedd84cdc8b537ffdaa3c517a438c
Mike van Es <github@depiraten.com>
2026-03-01 09:49:37 +0100

    Revert auth handler

--- apps/web/app/api/auth/[...all]/route.ts
+++ apps/web/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
 import { auth } from "@norish/auth/auth";
-import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = toNextJsHandler(auth);
+export const GET = auth.handler;
+export const POST = auth.handler;
