68e970391c42a0e69594795e9ebff15b7c4b8785
Giuseppe <mangoppola@outlook.com>
2026-04-04 18:57:58 +0200

    Prima versione traduzione in italiano

--- packages/config/src/server-config-loader.ts
+++ packages/config/src/server-config-loader.ts
@@ -244,6 +244,7 @@ export const DEFAULT_LOCALE_CONFIG: I18nLocaleConfig = {
     ko: { name: "한국어", enabled: true },
     pl: { name: "Polski", enabled: true },
     da: { name: "Dansk", enabled: true },
+    it: { name: "Italiano", enabled: true},
   },
 };
 
