cc975eb98c2763b7eeeafc04ba0c9d8db0378863
Mike van Es <github@depiraten.com>
2026-05-03 16:19:58 +0200

    Fix config issues

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -60,7 +60,7 @@ export default withNextIntl({
   env: {
     NEXT_PUBLIC_APP_VERSION: packageJson.version,
   },
-  serverExternalPackages: ["pino", "pino-pretty", "thread-stream", "playwright-core", "ws"],
+  serverExternalPackages: ["pino", "pino-pretty", "thread-stream", "playwright-core"],
   async headers() {
     return [
       {
