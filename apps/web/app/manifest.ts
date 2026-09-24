95fca50f40b2f040623129ee1a836557b0e90c03
Luca <45944543+AfoxDesignz@users.noreply.github.com>
2026-09-24 09:14:47 +0200

    fix: remove theme color from manifest (#576)

--- apps/web/app/manifest.ts
+++ apps/web/app/manifest.ts
@@ -10,7 +10,6 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#FFFEF7",
-    theme_color: "#FFFEF7",
     screenshots: [
       {
         src: "/mockup-norish.png",
